36171d42a1623989e2a8b1f5a5deb417ff75cc5c
taekjoo <taekjoo@gmail.com>
2015-01-21 12:03:10 +0900

    read raw file and then add modified publisher name

---
+++ mat.rb
@@ -0,0 +1,24 @@
+require 'csv'
+
+rawdata = []
+CSV.foreach("publisherandmatchypte_raw.csv", headers: true) do |row|
+  row['publisher'] =
+    case row['publisher.name']
+    when /Daum/ then 'daum'
+    when /Kakao/ then 'kakao'
+    when /Cauly/ then 'cauly'
+    when /SnD/ then 'snd'
+    when /Adbay/ then 'adbay'
+    when /Lette/ then 'lattescreen'
+    when /Google/ then 'google'
+    when /Coupang/ then 'coupang'
+    when /Naver/ then 'naver'
+    when /Band/ then 'band'
+    else 'etc'
+    end
+  rawdata << row
+end
+
+CSV.open("processed_data.csv", 'w') do |output|
+  rawdata.each { |row| output << row }
+end
